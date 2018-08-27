878101b80384fd5ef2a3718d2b104977349407e1
Jason Crescenzo <crescenzojay@gmail.com>
2018-08-27 19:27:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(arr1, arr2){
+  var empty = [];
+  for(var i = 0; i<arr1.length; i++){
+    empty.push(arr1[i]+" "+"plays the"+" "+arr2[i]);
+  }
+  return empty;
+}
