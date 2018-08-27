e7540122a90c82f039556a4571310e1bdeab4190
Jason Crescenzo <crescenzojay@gmail.com>
2018-08-27 19:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(arr1, arr2){
   var empty = [];
   for(var i = 0; i<arr1.length; i++){
-    empty.push(arr1[i]+" "+"plays the"+" "+arr2[i]);
+    empty.push(arr1[i]+" "+"plays"+" "+arr2[i]);
   }
   return empty;
 }
